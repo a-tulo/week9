a1115a8fab45544842d81bf39ca0065be4d806ff
alexandre.tulodziecki <alexandre.tulodziecki@yunextraffic.com>
2023-11-22 11:23:28 +0000

    Updated planet class to use a dictionary

--- planet.py
+++ planet.py
@@ -1,27 +1,29 @@
 class Planet:
 
-    def __init__(self, humans = [], robots = [], name = ""):
-        self.humans = humans
-        self.robots = robots
+    def __init__(self, name = ""):
         self.name = name
+        self.inhabitants = {
+        'humans': [],
+        'robots': []
+        }
 
     def add_human(self, human):
-        self.humans.append(human)
+        self.inhabitants['humans'].append(human)
 
     def remove_human(self, human):
-        self.humans.remove(human)
+        self.inhabitants['humans'].remove(human)
 
     def add_robot(self, robot):
-        self.robots.append(robot)
+        self.inhabitants['robots'].append(robot)
 
     def remove_robot(self, robot):
-        self.robots.remove(robot)
+        self.inhabitants['robots'].remove(robot)
 
     def __repr__(self):
-        return f'Humans: {self.humans} \n Robots: {self.robots}'
+        return f'Humans: {self.inhabitants['humans']} \n Robots: {self.inhabitants['robots']}'
     
     def __str__(self):
-        return f'List of humans are {self.humans}, and the list of robots are {self.robots}'
+        return f'List of humans are {self.inhabitants['humans']}, and the list of robots are {self.inhabitants['robots']}'
     
 if __name__ == "__main__":
     planet = Planet()
